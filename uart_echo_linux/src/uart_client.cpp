51ebf8218d5303c3f44b05cdc99133575a12ce9a
Sean Sovine <sean.r.sovine@gmail.com>
2026-08-21 15:18:09 -0400

    C++ UART client: Reorganize and add single-send test function

--- uart_echo_linux/src/uart_client.cpp
+++ uart_echo_linux/src/uart_client.cpp
@@ -1,106 +1,36 @@
-#include <chrono>
+#include "uart_lib.hpp"
+
 #include <cstdint>
 #include <cstring>
-#include <fcntl.h>
-#include <iostream>
-#include <string>
-#include <thread>
 
+#include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
 
+enum class Mode : uint32_t {
+    EchoRepeat,
+    SendOnce,
+};
+
+static constexpr Mode MODE             = Mode::EchoRepeat;
+static constexpr const char *PORT_NAME = "/dev/ttyUSB0";
+
 int main() {
-    const char *portname = "/dev/ttyUSB0";
-
-    // NOTE: Parameters below suggested by Gemini. vetted bY us.
-
-    // File descriptor parameters:
-    //  read/write; cannot be controlling terminal;
-    //  writes block until data + metadata are flushed
-    int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) {
-        std::cerr << "Error opening " << portname << std::endl;
+    std::optional<TtyPort> port = TtyPort::create(PORT_NAME);
+    if (!port) {
         return 1;
     }
 
-    struct termios tty;
-    if (tcgetattr(fd, &tty) != 0) {
-        std::cerr << "Error from tcgetattr" << std::endl;
-        close(fd);
-        return 1;
-    }
-
-    // Set Baud Rate to 115200.
-    cfsetispeed(&tty, B115200);
-    cfsetospeed(&tty, B115200);
-
-    // Control modes math configuration on board end:
-    //  8 bits, no parity, 1 stop bit;
-    //  disable hardware flow control.
-    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit characters
-    tty.c_cflag &= ~PARENB;                     // No parity
-    tty.c_cflag &= ~CSTOPB;                     // 1 stop bit
-    tty.c_cflag &= ~CRTSCTS;                    // No flow control
-    tty.c_cflag |= CREAD | CLOCAL;              // Turn on READ & ignore ctrl lines
-
-    // Local modes:
-    //  Raw input (disable canonical mode, echo, signals)
-    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-    // Input modes:
-    //  No software flow control. (TODO: Read up on it.)
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-
-    // Output modes:
-    //  Raw output; no post-processing.
-    tty.c_oflag &= ~OPOST;
-
-    // Read settings:
-    //  block until at least 1 byte arrives, w/ 0.5 s timeout.
-    tty.c_cc[VMIN]  = 0;
-    tty.c_cc[VTIME] = 5; // 0.5 seconds timeout
-
-    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-        std::cerr << "Error from tcsetattr" << std::endl;
-        close(fd);
-        return 1;
-    }
-
-    // Test string and receive buffer (only using 1 byte).
-    std::string msg = "Hello UART!";
-    char rxBuf[2];
-
-    /*
-     * Note on client/server protocol:
-     *
-     *  The "server" on te board receives on byte at a time
-     *  and then immediately echos it back. We send then
-     *  receive here based on that expectation.
-     */
-
-    static constexpr uint32_t NUM_TRANS = 100;
-
-    for (uint32_t trans = 0; trans < NUM_TRANS; ++trans) {
-        std::cout << "\n>> Sending message " << trans << ":" << std::endl;
-
-        for (const char ch : msg) {
-            // Send one char.
-            write(fd, (void *)&ch, 1);
-            std::cout << "Sent: " << ch << std::endl;
-
-            // Try to receive one char.
-            memset(rxBuf, 0, sizeof(rxBuf));
-            int n = read(fd, rxBuf, 1);
-            if (n > 0) {
-                std::cout << "Received: " << rxBuf[0] << std::endl;
-            } else {
-                std::cout << "No data received." << std::endl;
-            }
+    switch (MODE) {
+        case Mode::EchoRepeat: {
+            echo_test_repeat(port->fd());
+            break;
         }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        case Mode::SendOnce: {
+            send_once(port->fd());
+            break;
+        }
     }
 
-    close(fd);
     return 0;
 }
---
+++ uart_echo_linux/src/uart_lib.hpp
@@ -0,0 +1,146 @@
+#ifndef UART_LIB_HPP_
+#define UART_LIB_HPP_
+
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <thread>
+
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+// -------------------------------------------
+// Class representing Linux TTY serial device.
+
+class TtyPort {
+public:
+    TtyPort(int file_descriptor, struct termios tty)
+        : file_descriptor_(file_descriptor),
+          tty_(tty) {
+    }
+
+    ~TtyPort() {
+        close(file_descriptor_);
+    }
+
+    static std::optional<TtyPort> create(const std::string &portName) {
+        // File descriptor parameters:
+        //  read/write; cannot be controlling terminal;
+        //  writes block until data + metadata are flushed
+        int fd = open(portName.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
+        if (fd < 0) {
+            std::cerr << "Error opening " << portName << std::endl;
+            return std::nullopt;
+            ;
+        }
+
+        struct termios tty;
+        if (tcgetattr(fd, &tty) != 0) {
+            std::cerr << "Error from tcgetattr" << std::endl;
+            close(fd);
+            return std::nullopt;
+            ;
+        }
+
+        // Set Baud Rate to 115200.
+        cfsetispeed(&tty, B115200);
+        cfsetospeed(&tty, B115200);
+
+        // Control modes math configuration on board end:
+        //  8 bits, no parity, 1 stop bit;
+        //  disable hardware flow control.
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit characters
+        tty.c_cflag &= ~PARENB;                     // No parity
+        tty.c_cflag &= ~CSTOPB;                     // 1 stop bit
+        tty.c_cflag &= ~CRTSCTS;                    // No flow control
+        tty.c_cflag |= CREAD | CLOCAL;              // Turn on READ & ignore ctrl lines
+
+        // Local modes:
+        //  Raw input (disable canonical mode, echo, signals)
+        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
+        // Input modes:
+        //  No software flow control. (TODO: Read up on it.)
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+
+        // Output modes:
+        //  Raw output; no post-processing.
+        tty.c_oflag &= ~OPOST;
+
+        // Read settings:
+        //  block until at least 1 byte arrives, w/ 0.5 s timeout.
+        tty.c_cc[VMIN]  = 0;
+        tty.c_cc[VTIME] = 5; // 0.5 seconds timeout
+
+        if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+            std::cerr << "Error from tcsetattr" << std::endl;
+            close(fd);
+            return std::nullopt;
+            ;
+        }
+
+        return TtyPort{fd, tty};
+    }
+
+    int fd() {
+        return file_descriptor_;
+    }
+
+private:
+    int file_descriptor_;
+    struct termios tty_;
+};
+
+// ---------------
+// Test functions.
+
+inline void echo_test_repeat(int file_descriptor) {
+    // Test string and receive buffer (only using 1 byte).
+    std::string msg = "Hello UART!";
+    char rxBuf[2];
+
+    /*
+     * Note on client/server protocol:
+     *
+     *  The "server" on te board receives on byte at a time
+     *  and then immediately echos it back. We send then
+     *  receive here based on that expectation.
+     */
+
+    static constexpr uint32_t NUM_TRANS = 100;
+
+    for (uint32_t trans = 0; trans < NUM_TRANS; ++trans) {
+        std::cout << "\n>> Sending message " << trans << ":" << std::endl;
+
+        for (const char ch : msg) {
+            // Send one char.
+            write(file_descriptor, (void *)&ch, 1);
+            std::cout << "Sent: " << ch << std::endl;
+
+            // Try to receive one char.
+            memset(rxBuf, 0, sizeof(rxBuf));
+            int n = read(file_descriptor, rxBuf, 1);
+            if (n > 0) {
+                std::cout << "Received: " << rxBuf[0] << std::endl;
+            } else {
+                std::cout << "No data received." << std::endl;
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+inline void send_once(int file_descriptor) {
+    uint8_t sendData = 0x0F;
+    std::cout << "Sending byte: 0x" << std::hex << std::setw(2) << std::setfill('0') << (unsigned)sendData << std::endl;
+    write(file_descriptor, (void *)&sendData, 1);
+    std::cout << " - Send complete." << std::endl;
+}
+
+#endif // UART_LIB_HPP_
