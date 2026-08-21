2920f0745616b24f4e7d9ae79334b611f4492e9f
Sean Sovine <sean.r.sovine@gmail.com>
2026-08-21 15:23:07 -0400

    Work in progress to receive UART in DE10 Lite

--- uart_echo_de10_lite/led_blinker.v
+++ uart_echo_de10_lite/led_blinker.v
@@ -1,10 +1,10 @@
 module led_blinker #(
-      parameter CLK_FREQ   = 50_000_000,
-      parameter BLINK_RATE = 10
+    parameter CLK_FREQ   = 50_000_000,
+    parameter BLINK_RATE = 10
 ) (
-      input  wire clk,     // System clock 1.
-      input  wire rst_n,   // Active-low, async reset (pushbutton 0).
-      output reg [9:0] LED // LEDR0 - LEDR9.
+    input  wire clk,     // System clock 1.
+    input  wire rst_n,   // Active-low, async reset (pushbutton 0).
+    output reg [9:0] LED // LEDR0 - LEDR9.
 );
 
     // 32-bit register to count clock cycles.
@@ -27,6 +27,35 @@ module led_blinker #(
                 LED[current_led] <= 1'b0;
                 LED[next_led]    <= 1'b1;
                 current_led      <= next_led;
+            end else begin
+                clk_counter <= clk_counter + 1'b1;
+            end
+        end
+    end
+
+endmodule
+
+module led_blinker_single #(
+    parameter CLK_FREQ   = 50_000_000,
+    parameter BLINK_RATE = 10
+) (
+    input  wire clk,     // System clock 1.
+    input  wire rst_n,   // Active-low, async reset (pushbutton 0).
+    output reg  LED // LEDR0 - LEDR9.
+);
+
+    // 32-bit register to count clock cycles.
+    reg [31:0] clk_counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            clk_counter <= 32'd0;
+            LED         <= 10'b0;
+        end else begin
+            // Toggle next illuminated LED every 1 second.
+            if (clk_counter >= (CLK_FREQ / BLINK_RATE - 1)) begin
+                clk_counter <= 32'd0;
+                LED         <= ~LED;
             end else begin
                 clk_counter <= clk_counter + 1'b1;
             end
--- uart_echo_de10_lite/uart_echo.v
+++ uart_echo_de10_lite/uart_echo.v
@@ -1,20 +1,51 @@
 module uart_echo #(
-      parameter CLK_FREQ   = 50_000_000,
-      parameter BLINK_RATE = 10
+    parameter CLK_FREQ   = 50_000_000,
+    parameter BLINK_RATE = 10
 ) (
-      input wire clk,   // System clock 1.
-      input wire rst_n, // Active-low, async reset (pushbutton 0).
+    // System clock 1.
+    input wire clk,
+    // Active-low, async reset (pushbutton 0).
+    input wire rst_n,
+    // UART receive signal.
+    input wire uart_rx,
 
-      output wire [9:0] LED  // LEDR0 - LEDR9.
+    // LEDR9 - LEDR0.
+    output wire [9:0] LED,
+    // UART transmit signal.
+    output wire uart_tx
 );
-
-    // Blink LEDs to show design is running.
-    led_blinker blinker (
-          .clk  (clk),
-          .rst_n(rst_n),
-          .LED  (LED)
+    // Blink LED to show design is running.
+    led_blinker_single blinker (
+        .clk  (clk),
+        .rst_n(rst_n),
+        .LED  (LED[9])
     );
 
-    // TODO: Find UART Verilog IP and use it to build echo server.
+    // Current state of uart receiver data register.
+    wire [7:0] uart_rx_byte;
+    wire uart_rx_dv;
+    reg uart_received;
+
+    always @(posedge clk or negedge rst_n or posedge uart_rx_dv) begin
+        if (!rst_n) begin
+            uart_received <= 1'b0;
+        end else begin
+            uart_received <= uart_received | uart_rx_dv;
+        end
+    end
+
+    uart_rx #(
+        // Set to CLK_FREQ / 115_200.
+        .CLKS_PER_BIT(434)
+    ) uart_receiver (
+        .i_Clock(clk),
+        .i_Rx_Serial(uart_rx),
+        .o_Rx_DV(uart_rx_dv),
+        .o_Rx_Byte(uart_rx_byte)
+    );
+
+    // LEDs 7 to 0 show start of UART receive byte register.
+    assign LED[7:0] = uart_rx_byte;
+    assign LED[8]   = uart_received;
 
 endmodule
